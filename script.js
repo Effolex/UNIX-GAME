232c3f5f789d4d991b6e07818a30d0bb2e4fdc58
Effolex <32694502+Effolex@users.noreply.github.com>
2021-07-14 12:20:20 -0300

    update script.js
    correção de questões e adicionado mais uma opção

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 		var answers = ["ls", "ls -a", "mkdir", "cd pasta1", 
 			"cd", "cd ..", "pwd", "cp arq1 arq2", "mv arq1 arq2", 
 			"rm arq", "rmdir pasta1", "cat arq", "less arq", "head arq", 
-			"tail arq", "wc arq", "*","man help", "whatis help"];
+			"tail arq", "wc arq", "*" , "?","man help", "whatis help"];
 		var questions = ["lista arquivos e diretorios", 
 			"lista TODOS os arquivos e diretorios", "cria um diretorio", 
 			"muda para o diretório chamado pasta1", "muda para o diretório home quando passado sem argumentos",
 			"muda para o diretorio pai", "mostra o caminho do diretorio atual",
 			"copia arq1 e o chama de arq2", "move ou renomeia arq1 para arq2",
 			"remove o arquivo chamado arq", "remove o diretorio chamado pasta1",
-			"exibe o arquivo chamado arq", "exibe um arquivo chamado arq, uma página por vez",
+			"le o conteudo do arquivo chamado arq e exibe no terminal", "exibe um arquivo chamado arq, uma página por vez",
 			"exibe as primeiras linhas do arquivo arq", "exibe as ultimas linhas do arquivo arq", "conta o numero de linhas / palavras / caracteres no arquivo arq",
-			"corresponde a um caracter", "le a pagina do manual online do comando help", "breve descrição do comando help" ];
+			"corresponde a um ou mais caracteres","corresponde a um UNICO caracter", "le a pagina do manual online do comando help", "breve descrição do comando help" ];
 		
 
 var userConsole = document.getElementById("console");
